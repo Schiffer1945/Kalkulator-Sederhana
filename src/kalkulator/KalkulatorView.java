0717e3eeb4bd8effcb7782257666b1121784c166
Fariz <Schiffer_1945@yahoo.com>
2017-05-27 09:53:23 +0700

    fix

--- src/kalkulator/KalkulatorView.java
+++ src/kalkulator/KalkulatorView.java
@@ -10,12 +10,21 @@ package kalkulator;
  * @author Fariz
  */
 public class KalkulatorView extends javax.swing.JFrame {
-
+        String angka;
+        double jumlah, angka1, angka2;
+        int pilih;
     /**
      * Creates new form KalkulatorView
      */
     public KalkulatorView() {
         initComponents();
+        
+        display.setText("");
+        angka = "";
+        angka1 = 0.0;
+        angka2 = 0.0;
+        jumlah = 0.0;
+        
     }
 
     /**
@@ -27,7 +36,7 @@ public class KalkulatorView extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jTextField1 = new javax.swing.JTextField();
+        display = new javax.swing.JTextField();
         btn9 = new javax.swing.JButton();
         btn7 = new javax.swing.JButton();
         btn8 = new javax.swing.JButton();
@@ -44,89 +53,290 @@ public class KalkulatorView extends javax.swing.JFrame {
         btnKali = new javax.swing.JButton();
         btnKurang = new javax.swing.JButton();
         btnTambah = new javax.swing.JButton();
-        btnClear = new javax.swing.JButton();
+        jButton17 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
         getContentPane().setLayout(null);
 
-        jTextField1.setEditable(false);
-        jTextField1.setBackground(new java.awt.Color(255, 255, 255));
-        jTextField1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jTextField1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        jTextField1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        getContentPane().add(jTextField1);
-        jTextField1.setBounds(10, 11, 380, 60);
+        display.setEditable(false);
+        display.setBackground(new java.awt.Color(255, 255, 255));
+        display.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        display.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
+        display.setToolTipText("");
+        display.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        display.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        getContentPane().add(display);
+        display.setBounds(10, 11, 380, 60);
 
         btn9.setText("9");
+        btn9.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn9ActionPerformed(evt);
+            }
+        });
         getContentPane().add(btn9);
-        btn9.setBounds(170, 90, 39, 50);
+        btn9.setBounds(170, 90, 70, 50);
 
         btn7.setText("7");
+        btn7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn7ActionPerformed(evt);
+            }
+        });
         getContentPane().add(btn7);
-        btn7.setBounds(10, 90, 39, 50);
+        btn7.setBounds(10, 90, 70, 50);
 
         btn8.setText("8");
+        btn8.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn8ActionPerformed(evt);
+            }
+        });
         getContentPane().add(btn8);
-        btn8.setBounds(90, 90, 39, 50);
+        btn8.setBounds(90, 90, 70, 50);
 
         btn4.setText("4");
+        btn4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn4ActionPerformed(evt);
+            }
+        });
         getContentPane().add(btn4);
-        btn4.setBounds(10, 150, 39, 50);
+        btn4.setBounds(10, 150, 70, 50);
 
         btn5.setText("5");
+        btn5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn5ActionPerformed(evt);
+            }
+        });
         getContentPane().add(btn5);
-        btn5.setBounds(90, 150, 39, 50);
+        btn5.setBounds(90, 150, 70, 50);
 
         btn6.setText("6");
+        btn6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn6ActionPerformed(evt);
+            }
+        });
         getContentPane().add(btn6);
-        btn6.setBounds(170, 150, 39, 50);
+        btn6.setBounds(170, 150, 70, 50);
 
         btn1.setText("1");
+        btn1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn1ActionPerformed(evt);
+            }
+        });
         getContentPane().add(btn1);
-        btn1.setBounds(10, 210, 39, 50);
+        btn1.setBounds(10, 210, 70, 50);
 
         btn2.setText("2");
+        btn2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn2ActionPerformed(evt);
+            }
+        });
         getContentPane().add(btn2);
-        btn2.setBounds(90, 210, 39, 50);
+        btn2.setBounds(90, 210, 70, 50);
 
         btn3.setText("3");
+        btn3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn3ActionPerformed(evt);
+            }
+        });
         getContentPane().add(btn3);
-        btn3.setBounds(170, 210, 39, 50);
+        btn3.setBounds(170, 210, 70, 50);
 
         btn0.setText("0");
+        btn0.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn0ActionPerformed(evt);
+            }
+        });
         getContentPane().add(btn0);
-        btn0.setBounds(10, 270, 39, 50);
+        btn0.setBounds(10, 270, 70, 50);
 
         jButton11.setText(",");
         getContentPane().add(jButton11);
-        jButton11.setBounds(90, 270, 37, 50);
+        jButton11.setBounds(90, 270, 70, 50);
 
         btnEqual.setText("=");
+        btnEqual.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEqualActionPerformed(evt);
+            }
+        });
         getContentPane().add(btnEqual);
-        btnEqual.setBounds(170, 270, 41, 50);
+        btnEqual.setBounds(170, 270, 70, 50);
 
         btnBagi.setText("/");
+        btnBagi.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBagiActionPerformed(evt);
+            }
+        });
         getContentPane().add(btnBagi);
-        btnBagi.setBounds(250, 270, 37, 50);
+        btnBagi.setBounds(250, 270, 70, 50);
 
         btnKali.setText("x");
+        btnKali.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnKaliActionPerformed(evt);
+            }
+        });
         getContentPane().add(btnKali);
-        btnKali.setBounds(250, 210, 39, 50);
+        btnKali.setBounds(250, 210, 70, 50);
 
         btnKurang.setText("-");
+        btnKurang.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnKurangActionPerformed(evt);
+            }
+        });
         getContentPane().add(btnKurang);
-        btnKurang.setBounds(250, 150, 37, 50);
+        btnKurang.setBounds(250, 150, 70, 50);
 
         btnTambah.setText("+");
+        btnTambah.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnTambahActionPerformed(evt);
+            }
+        });
         getContentPane().add(btnTambah);
-        btnTambah.setBounds(250, 90, 41, 50);
+        btnTambah.setBounds(250, 90, 70, 50);
 
-        btnClear.setText("C");
-        getContentPane().add(btnClear);
-        btnClear.setBounds(330, 270, 39, 50);
+        jButton17.setText("C");
+        jButton17.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton17ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton17);
+        jButton17.setBounds(330, 150, 60, 170);
 
-        pack();
+        setSize(new java.awt.Dimension(428, 379));
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btn0ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn0ActionPerformed
+        angka += "0";
+        display.setText(angka);
+    }//GEN-LAST:event_btn0ActionPerformed
+
+    private void btn1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn1ActionPerformed
+        angka += "1";
+        display.setText(angka);
+    }//GEN-LAST:event_btn1ActionPerformed
+
+    private void btn2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn2ActionPerformed
+        angka += "2";
+        display.setText(angka);
+    }//GEN-LAST:event_btn2ActionPerformed
+
+    private void btn3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn3ActionPerformed
+        angka += "3";
+        display.setText(angka);
+    }//GEN-LAST:event_btn3ActionPerformed
+
+    private void btn4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn4ActionPerformed
+        angka += "4";
+        display.setText(angka);
+    }//GEN-LAST:event_btn4ActionPerformed
+
+    private void btn5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn5ActionPerformed
+        angka += "5";
+        display.setText(angka);
+    }//GEN-LAST:event_btn5ActionPerformed
+
+    private void btn6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn6ActionPerformed
+        angka += "6";
+        display.setText(angka);
+    }//GEN-LAST:event_btn6ActionPerformed
+
+    private void btn7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn7ActionPerformed
+        angka += "7";
+        display.setText(angka);
+    }//GEN-LAST:event_btn7ActionPerformed
+
+    private void btn8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn8ActionPerformed
+        angka += "8";
+        display.setText(angka);
+    }//GEN-LAST:event_btn8ActionPerformed
+
+    private void btn9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn9ActionPerformed
+        angka += "9";
+        display.setText(angka);
+    }//GEN-LAST:event_btn9ActionPerformed
+
+    private void jButton17ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton17ActionPerformed
+        display.setText("");
+        angka = "";
+        angka1 = 0.0;
+        angka2 = 0.0;
+        jumlah = 0.0;
+    }//GEN-LAST:event_jButton17ActionPerformed
+
+    private void btnTambahActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnTambahActionPerformed
+        angka1 = Double.parseDouble(angka);
+        display.setText("+");
+        angka = "";
+        pilih = 1;
+    }//GEN-LAST:event_btnTambahActionPerformed
+
+    private void btnKurangActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnKurangActionPerformed
+        angka1 = Double.parseDouble(angka);
+        display.setText("-");
+        angka = "";
+        pilih = 2;
+    }//GEN-LAST:event_btnKurangActionPerformed
+
+    private void btnKaliActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnKaliActionPerformed
+        angka1 = Double.parseDouble(angka);
+        display.setText("x");
+        angka = "";
+        pilih = 3;
+    }//GEN-LAST:event_btnKaliActionPerformed
+
+    private void btnBagiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBagiActionPerformed
+        angka1 = Double.parseDouble(angka);
+        display.setText("/");
+        angka = "";
+        pilih = 4;
+    }//GEN-LAST:event_btnBagiActionPerformed
+
+    private void btnEqualActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEqualActionPerformed
+        switch(pilih){
+        case 1:
+            angka2 = Double.parseDouble(angka);
+            jumlah = angka1 + angka2;
+            angka = Double.toString(jumlah);
+            display.setText(angka);
+            break;
+        case 2:
+            angka2 = Double.parseDouble(angka);
+            jumlah = angka1 - angka2;
+            angka = Double.toString(jumlah);
+            display.setText(angka);
+            break;
+        case 3:
+            angka2 = Double.parseDouble(angka);
+            jumlah = angka1 * angka2;
+            angka = Double.toString(jumlah);
+            display.setText(angka);
+            break;
+        case 4:
+            angka2 = Double.parseDouble(angka);
+            jumlah = angka1 / angka2;
+            angka = Double.toString(jumlah);
+            display.setText(angka);
+            break;
+            default:
+            break;
+        } 
+    }//GEN-LAST:event_btnEqualActionPerformed
 
     /**
      * @param args the command line arguments
@@ -175,12 +385,12 @@ public class KalkulatorView extends javax.swing.JFrame {
     private javax.swing.JButton btn8;
     private javax.swing.JButton btn9;
     private javax.swing.JButton btnBagi;
-    private javax.swing.JButton btnClear;
     private javax.swing.JButton btnEqual;
     private javax.swing.JButton btnKali;
     private javax.swing.JButton btnKurang;
     private javax.swing.JButton btnTambah;
+    private javax.swing.JTextField display;
     private javax.swing.JButton jButton11;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JButton jButton17;
     // End of variables declaration//GEN-END:variables
 }
